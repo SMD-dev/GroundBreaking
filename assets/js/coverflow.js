7154bca7422d727dc4159df124163bf3200a15d7
Ryan Walker <RyanPrestonWalker@gmail.com>
2018-06-20 20:52:21 -0600

    Added coverflow and song lists

--- assets/js/coverflow.js
+++ assets/js/coverflow.js
@@ -1,16 +1,34 @@
 $(function() {
-  $("#coverflow").coverflow({
-    active: 3,
-    select: function(event, ui) {
-      console.log("here");
-    }
-  });
+  // for (let i = 0; i < 7; i++) {
+  //   $("#1" + i).slideToggle();
+  // }
 
-  $("#coverflow img").click(function() {
-    if (!$(this).hasClass("ui-state-active")) {
-      return;
-    }
+  $("#l3").slideToggle();
 
-    $("#coverflow").coverflow("next");
-  });
+  setTimeout(() => {
+    var prev = 3;
+    $("#coverflow").coverflow({
+      active: 3,
+      trigger: { mousewheel: false },
+      select: function(event, ui) {
+        $("#l" + prev).slideToggle(() => {
+          event.target.style.display = "block";
+          $("#l" + ui["index"]).slideToggle();
+          prev = ui["index"];
+        });
+      }
+    });
+
+    $("#coverflow img").click(function() {
+      if (!$(this).hasClass("ui-state-active")) {
+        return;
+      }
+
+      $("#coverflow").coverflow("next");
+    });
+  }, 3000);
 });
+
+// $("#coverflow").bind("mousewheel", function() {
+//   return false;
+// });
